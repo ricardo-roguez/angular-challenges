932cfc63daa2370e9b97a8544f73ccbcb1556fdb
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2023-08-29 08:54:32 +0200

    feat: add simple pipe

--- apps/pipe-easy/src/app/app.component.ts
+++ apps/pipe-easy/src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { SimplePipe } from './simple-pipe.pipe';
 
 @Component({
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, SimplePipe],
   selector: 'app-root',
   template: `
     <div *ngFor="let person of persons; let index = index">
-      {{ heavyComputation(person, index) }}
+      {{ person | simplePipe : index }}
     </div>
   `,
 })
 export class AppComponent {
   persons = ['toto', 'jack'];
-
-  heavyComputation(name: string, index: number) {
-    // very heavy computation
-    return `${name} - ${index}`;
-  }
 }
---
+++ apps/pipe-easy/src/app/simple-pipe.pipe.ts
@@ -0,0 +1,11 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'simplePipe',
+  standalone: true,
+})
+export class SimplePipe implements PipeTransform {
+  transform(name: string, index: number): string {
+    return `${name} - ${index}`;
+  }
+}
