4cb66fb1ca4ce5be0b48002691ae89c91a7ce1ec
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2023-11-16 09:16:47 +0100

    feat: use host context to style components

--- apps/styling/src/app/static-text.component.ts
+++ apps/styling/src/app/static-text.component.ts
@@ -1,33 +1,32 @@
 /* eslint-disable @angular-eslint/component-selector */
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { TextComponent } from './text.component';
-
-export type StaticTextType = 'normal' | 'warning' | 'error';
 
 @Component({
   selector: 'static-text',
   standalone: true,
   imports: [TextComponent],
-  template: `
-    <text [font]="font" [color]="color">This is a static text</text>
-  `,
-})
-export class TextStaticComponent {
-  @Input() set type(type: StaticTextType) {
-    switch (type) {
-      case 'error': {
-        this.font = 30;
-        this.color = 'red';
-        break;
+  styles: [
+    `
+      text {
+        --text-font-size: 10px;
+        --text-color: black;
       }
-      case 'warning': {
-        this.font = 25;
-        this.color = 'orange';
-        break;
-      }
-    }
-  }
 
-  font = 10;
-  color = 'black';
-}
+      :host-context(.error) {
+        & text {
+          --text-font-size: 30px;
+          --text-color: red;
+        }
+      }
+      :host-context(.warning) {
+        & text {
+          --text-font-size: 25px;
+          --text-color: orange;
+        }
+      }
+    `,
+  ],
+  template: ` <text>This is a static text</text> `,
+})
+export class TextStaticComponent {}
