8ac404b85de05fbe4fa79acfcecd389818a8fdf0
Ricardo Rodriguez <rrodriguez@voxelgroup.net>
2024-02-29 09:16:34 +0100

    feat: solution challenge 16

---
+++ apps/angular/di/src/app/currency-code.directive.ts
@@ -0,0 +1,15 @@
+﻿import { Directive, inject, Input } from '@angular/core';
+import { CurrencyService } from './currency.service';
+
+@Directive({
+  selector: '[code]',
+  providers: [CurrencyService],
+  standalone: true,
+})
+export class CurrencyCodeDirective {
+  currencyService = inject(CurrencyService);
+
+  @Input() set code(code: string) {
+    this.currencyService.patchState({ code });
+  }
+}
